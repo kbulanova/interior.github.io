35b036f8a9a5d438e4d526343c033d2c8cfb1078
kbulanova <135860160+kbulanova@users.noreply.github.com>
2023-06-07 19:55:57 +0300

    Add files via upload

---
+++ js/accord.js
@@ -0,0 +1,22 @@
+window.addEventListener("DOMContentLoaded", function () {
+	"use strict";
+    // Аккордеон
+    const accordion = () => {
+        const btns = document.querySelectorAll(".accordion-head");
+        btns.forEach((btn) => {
+            btn.addEventListener("click", function () {
+                this.classList.toggle("active-style");
+                //Следующий элемент
+                this.nextElementSibling.classList.toggle("active-content");
+                if (this.classList.contains("active-style")) {
+                    this.nextElementSibling.style.maxHeight =
+                        this.nextElementSibling.scrollHeight + 50 + "px";
+                } else {
+                    this.nextElementSibling.style.maxHeight = "0px";
+                }
+            });
+        });
+    };  
+
+    accordion();
+})
